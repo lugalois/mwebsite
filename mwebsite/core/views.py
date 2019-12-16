9b1d7dfe507dccffc403dbf533bb8d8effb353f2
luis <luisgonzalezm331@gmail.com>
2019-12-15 21:31:28 -0500

    templates inheritances

--- mwebsite/core/views.py
+++ mwebsite/core/views.py
@@ -1,35 +1,15 @@
-from django.shortcuts import render, HttpResponse
-
-html_base = """
-<h1>Mi web personal</h1>
-<ul>
-	<li><a href="/">Portada</a></li>
-	<li><a href="/about-me/">Acerca de mi</a></li>
-	<li><a href="/portfolio/">Portafolio</a></li>
-	<li><a href="/contact/">Contacto</a></li>
-</ul>
-"""
-
+from django.shortcuts import render
 
 def home(request):	
 	return render(request, "core/home.html")
 
 def about(request):
-	return HttpResponse(html_base+"""
-		<h2>acerca de</h2>
-		<p>Me llamo Luis y soy Cienctifico de la computadora</p>
-		""")
+	return render(request, "core/aboutme.html")
 
 def portfolio(request):	
-	return HttpResponse(html_base+"""
-		<h2>Portafolio</h2>
-		<p>Alguno de mis trabajos</p>
-		""")
+	return render(request, "core/portfolio.html")
 
 def contact(request):
-	return HttpResponse(html_base+"""
-		<h2>Contacto</h2>
-		<p>gordototuma@gmail.com</p>
-		""")
+	return render(request, "core/contact.html")
 
 
